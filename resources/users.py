75067e21f8804a553abec99d2995fce4323498d5
peaton373 <peaton373@gmail.com>
2020-05-11 13:56:30 -0500

    created login route for user

--- resources/users.py
+++ resources/users.py
@@ -55,3 +55,43 @@ def register():
 			message=f"Successfully registered user {created_user_dict['email']}",
 			status=201
 		), 201
+
+# login
+@users.route('/login', methods=['POST'])
+def login():
+	payload = request.get_json()
+	payload['email'] = payload['email'].lower()
+	payload['username'] = payload['username'].lower()
+
+	try:
+		user = models.User.get(models.User.email == payload['email'])
+		user_dict = model_to_dict(user)
+		password_is_good = check_password_hash(user_dict['password'], payload['password'])
+
+		if(password_is_good):
+			login_user(user)
+			print(model_to_dict(user))
+
+			user_dict.pop('password')
+
+			return jsonify(
+				data=user_dict,
+				message=f"Successfully logged in {user_dict['email']}",
+				status=201
+			), 201
+
+		else:
+			print('pw is no good here')
+			return jsonify(
+				data={},
+				message="Email or password is incorrect",
+				status=401
+			), 401
+
+	except models.DoesNotExist:
+		print('username is no good here')
+		return jsonify(
+			data={},
+			message="Email or password is incorrect",
+			status=401
+		), 401
